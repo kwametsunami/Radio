0f128e7912f39f1a1391e2f41789fbbeb761200d
kwametsunami <kwame.kubi@hotmail.com>
2022-11-01 10:01:34 -0400

    fixed useEffect in station component

--- src/App.js
+++ src/App.js
@@ -1,63 +1,12 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-// import AudioPlayer from 'react-h5-audio-player';
-// import 'react-h5-audio-player/lib/styles.css';
 import Genre from './Genre';
-import Stations from './Stations';
-
 
 function App() {
 
-  const [ allStations, setAllStations] = useState([])
-  const [ filteredStations, setFilteredStations] = useState([])
-
-  const getStations = (event, genreSelector) => {
-    event.preventDefault();
-    const allStationsCopy = [...allStations]
-    const stationByTag = allStationsCopy.filter((station) => {
-      for (let i = 0; i < allStationsCopy.length; i++){
-        return allStationsCopy[i] === genreSelector
-      }
-      return station.tags === genreSelector
-    })
-      // for (let i = 0; i < allStationsCopy.length; i++){
-      //   stationByTag.push(allStationsCopy.tags === '80s')
-      // }
-    console.log(stationByTag)
-    console.log(genreSelector)
-  }
-
-  
-  useEffect(() => {
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': '8e4a9bbd26msh31ec88528a1bb51p195af6jsn4f715f3b0e33',
-        'X-RapidAPI-Host': 'radio-browser.p.rapidapi.com'
-      }
-    };
-
-    fetch('https://radio-browser.p.rapidapi.com/json/stations/search?&bitrateMin=192&reverse=false&offset=0&limit=100&hidebroken=false', options)
-      // countrycode=ca&tag=jazz
-      .then(response => response.json(), console.log("heyyyyy"))
-      .then(info => {
-        console.log(info)
-        const radioStations = info
-
-        setAllStations(radioStations)
-        console.log(radioStations[0].tags)
-      })
-      .catch((error) => {
-        // app.getApi()
-        console.log("error")
-      });
-  }, []) 
-
   return (
     <div className="App">
-      <h1>radio test</h1>
-      <Genre chooseGenre = {getStations}/>
-      <Stations stations = {allStations} />
+      <h1>International Radio</h1>
+      <Genre />
     </div>
   );
 }
--- src/Genre.js
+++ src/Genre.js
@@ -1,26 +1,54 @@
 import { useState } from 'react'
+import Stations from './Stations'
 
-const Genre = (props) => {
+const Genre = () => {
 
-    const [ userChoice, setUserChoice ] = useState('placeholder')
+    const [userChoice, setUserChoice] = useState()
 
     const handleUserChoice = (event) => {
         setUserChoice(event.target.value)
+        // console.log(userChoice)
     }
 
     return (
-        <form onSubmit={ (event) => props.chooseGenre(event, userChoice) }>
-            <h2>Select a genre</h2>
-            <select name="genreSelector" id="genreSelector" onChange={handleUserChoice} value={userChoice}>
-                <option value="placeholder" disabled>Genre</option>
-                <option value="pop">Pop</option>
-                <option value="rock">Rock</option>
-                <option value="jazz">Jazz</option>
-                <option value="hiphop">Hip Hop</option>
-                <option value="retro">Retro</option>
-            </select>
-            <button type="submit">Display stations</button>
-        </form>
+        <section>
+            <form>
+                <h2>Choose a genre</h2>
+                <select name="genreSelector" id="genreSelector" onChange={handleUserChoice} value={userChoice} defaultValue={""}>
+                    <option value="" disabled>Genre</option>
+                    <option value="pop">Pop</option>
+                    <option value="rock">Rock</option>
+                    <option value="indie">Indie</option>
+                    <option value="electronic">Electronic</option>
+                    <option value="house">House</option>
+                    <option value="dance">Dance</option>
+                    <option value="hiphop">Hip Hop</option>
+                    <option value="rnb">R&B</option>
+                    <option value="reggae">Reggae</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="classical">Classical</option>
+                    <option value="folk">Folk</option>
+                    <option value="country">Country</option>
+                    <option value="blues">Blues</option>
+                    <option value="punk">Punk</option>
+                    <option value="metal">Metal</option>
+                    <option value="experimental">Experimental</option>
+                    <option value="community">Community</option>
+                    <option value="60s">60s</option>
+                    <option value="70s">70s</option>
+                    <option value="80s">80s</option>
+                    <option value="90s">90s</option>
+                    <option value="talk">Talk Radio</option>
+                    <option value="news">News</option>
+                    <option value="sport">Sports</option>
+                </select>
+            </form>
+            {
+                userChoice
+                    ? <Stations genre={userChoice} />
+                    : null
+            }
+        </section>
     )
 }
 
